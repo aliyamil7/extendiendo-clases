4b7d0c3d3d5695b147045152bbf78c8f3d8978d8
Yamil Ali <Yamil8787@gmail.com>
2025-07-24 12:57:08 -0300

    Todos los test pasaron

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+import productsData from "./products.json";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
   constructor(name: string) {
-    // nombre de esta lista
     this.name = name;
   }
-  add(nuevaCosa) {
+  add(nuevaCosa: any) {
     this.cosas.push(nuevaCosa);
   }
   getCosas() {
@@ -24,6 +25,40 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    this.cosas = productsData;
+  }
+
+  addProduct(newProduct: Product) {
+    this.cosas.push(newProduct);
+  }
+
+  getProduct(id: number): Product | undefined {
+    for (let i = 0; i < this.cosas.length; i++) {
+      if (this.cosas[i].id === id) {
+        return this.cosas[i];
+      }
+    }
+    return undefined;
+  }
+
+  removeProduct(id: number) {
+    this.cosas = this.cosas.filter((p) => p.id !== id);
+  }
+
+  getSortedByPrice(order: "asc" | "desc" = "asc"): Product[] {
+    const copia = [...this.cosas];
+    copia.sort((a, b) => {
+      if (order === "asc") {
+        return a.price - b.price;
+      } else {
+        return b.price - a.price;
+      }
+    });
+    return copia;
+  }
+}
 
 export { ListaDeProductos, Product };
